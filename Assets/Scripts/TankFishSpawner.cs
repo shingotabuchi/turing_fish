ccc46f6f1007334393b8f5f8fc4e4226e90af63e
DESKTOP-UAS5MBE\KIBOU <tabuchi.shingo.76m@st.kyoto-u.ac.jp>
2023-01-18 04:21:36 +0900

    hmm

--- Assets/Scripts/TankFishSpawner.cs
+++ Assets/Scripts/TankFishSpawner.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class TankFishSpawner : MonoBehaviour
 {
     public int carpSpawnCount;
-    public GameObject carp;
+    public GameObject[] carps;
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < carpSpawnCount; i++)
         {
-            GameObject newCarp = Instantiate(carp,transform);
+            GameObject newCarp = Instantiate(carps[Random.Range(0,carps.Length)],transform);
             Transform tank = transform.Find("Tank");
             newCarp.transform.position = tank.position + tank.localScale.y*5f*Vector3.up;
             newCarp.transform.rotation = Quaternion.Euler(0,Random.Range(0f,360f),0);
