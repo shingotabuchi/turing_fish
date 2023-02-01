332384d403226e00fe0c040dc8f2acb261921dc5
DESKTOP-UAS5MBE\KIBOU <tabuchi.shingo.76m@st.kyoto-u.ac.jp>
2023-02-01 22:53:42 +0900

    a

--- Assets/Scripts/LBM3DAccurate.cs
+++ Assets/Scripts/LBM3DAccurate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
-// using System.Runtime.InteropServices;
+
 public class LBM3DAccurate : MonoBehaviour
 {
     public enum ForceType
@@ -24,16 +24,25 @@ public class LBM3DAccurate : MonoBehaviour
     public float u0;
     public float rho0 = 1.0f;
     public float Re = 150f;
-    public float forceRadius,forceScaler;
+    public float tangentialForceRadius,tangentialForceScaler;
+    public float radialForceRadius,radialForceScaler;
     float nu,tau,omega;
-    int setRandom,initVortex,initZero,collisionsVortex,streaming,boundaries,periodicBoundaries,collisionsCenter;
+    int setRandom,initVortex,initZero,collisions,streaming,boundaries,periodicBoundaries;
     ComputeBuffer f;
     Vector3[] velocityArray;
     GraphicsBuffer velocityGraphicsBuffer;
     public int loopCount = 1;
     public bool tangentialForceIsOn;
+    public bool radialForceIsOn;
+    public UICanvas uiCanvas;
+
+    Ray ray;
+    RaycastHit hit;
     
-    private void Start() {
+    bool isTouched;
+    
+    private void Start()
+    {
         vfx = GetComponent<VisualEffect>();
         velocityGraphicsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, DIM*DIM*DIM, sizeof(float)*3);
         vfx.SetGraphicsBuffer("VelocityBuffer",velocityGraphicsBuffer);
@@ -42,14 +51,12 @@ public class LBM3DAccurate : MonoBehaviour
 
         initVortex = compute.FindKernel("InitVortex");
         initZero = compute.FindKernel("InitZero");
-        collisionsVortex = compute.FindKernel("Collisions");
-        collisionsCenter = compute.FindKernel("CollisionsCenterForce");
+        collisions = compute.FindKernel("Collisions");
         streaming = compute.FindKernel("Streaming");
         boundaries = compute.FindKernel("Boundaries");
         periodicBoundaries = compute.FindKernel("PeriodicBoundaries");
 
         compute.SetInt("DIM",DIM);
-        compute.SetBool("tangentialForceIsOn",tangentialForceIsOn);
 
         SetVariables();
 
@@ -59,10 +66,8 @@ public class LBM3DAccurate : MonoBehaviour
         compute.SetBuffer(initZero,"velocityBuffer",velocityGraphicsBuffer);
         compute.SetBuffer(initZero,"f",f);
         
-        compute.SetBuffer(collisionsVortex,"velocityBuffer",velocityGraphicsBuffer);
-        compute.SetBuffer(collisionsVortex,"f",f);
-        compute.SetBuffer(collisionsCenter,"velocityBuffer",velocityGraphicsBuffer);
-        compute.SetBuffer(collisionsCenter,"f",f);
+        compute.SetBuffer(collisions,"velocityBuffer",velocityGraphicsBuffer);
+        compute.SetBuffer(collisions,"f",f);
         compute.SetBuffer(streaming,"f",f);
         compute.SetBuffer(boundaries,"f",f);
         compute.SetBuffer(periodicBoundaries,"f",f);
@@ -71,23 +76,44 @@ public class LBM3DAccurate : MonoBehaviour
         if(initType == InitType.Vortex) compute.Dispatch(initVortex,(DIM+7)/8,(DIM+7)/8,(DIM+7)/8);
     }
 
+    public void Initialize()
+    {
+        compute.Dispatch(initZero,(DIM+7)/8,(DIM+7)/8,(DIM+7)/8);
+    }
+
     private void Update() {
-        // SetRandomPixelsCompute();
+        if(Input.GetMouseButton(0))
+        {
+            if(uiCanvas.sideCamera.enabled) ray = uiCanvas.sideCamera.ScreenPointToRay(Input.mousePosition);
+            else ray = uiCanvas.topCamera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit,Mathf.Infinity,LayerMask.GetMask("HitBox")))
+            {
+                if(!isTouched)
+                {
+                    isTouched = true;
+                    compute.SetBool("isTouched",isTouched);
+                }
+                compute.SetVector("touchTextureCoord",hit.textureCoord);
+            }
+            else if(isTouched)
+            {
+                isTouched = false;
+                compute.SetBool("isTouched",isTouched);
+            }
+        }
+        else if(isTouched)
+        {
+            isTouched = false;
+            compute.SetBool("isTouched",isTouched);
+        }
+
         for (int k = 0; k < loopCount; k++)
         {
-            if(forceType == ForceType.Vortex){
-                // compute.SetBuffer(collisionsVortex,"velocityBuffer",velocityGraphicsBuffer);
-                compute.Dispatch(collisionsVortex,(DIM+7)/8,(DIM+7)/8,(DIM+7)/8);
-            }
-            if(forceType == ForceType.Center){
-                compute.Dispatch(collisionsCenter,(DIM+7)/8,(DIM+7)/8,(DIM+7)/8);
-            }
+            compute.Dispatch(collisions,(DIM+7)/8,(DIM+7)/8,(DIM+7)/8);
             compute.Dispatch(streaming,(DIM+7)/8,(DIM+7)/8,(DIM+7)/8);
             if(periodic) compute.Dispatch(periodicBoundaries,(DIM+7)/8,(DIM+7)/8,1);
             else compute.Dispatch(boundaries,(DIM+7)/8,(DIM+7)/8,1);
         }
-        // tex3D.SetPixels(velocityGraphicsBuffer);
-        // tex3D.Apply();
     }
 
     private void OnValidate() {
@@ -95,14 +121,26 @@ public class LBM3DAccurate : MonoBehaviour
     }
 
     void SetVariables()
-    {
+    {   
+        tangentialForceIsOn = uiCanvas.tangentialForceToggle.isOn;
+        tangentialForceScaler = uiCanvas.tangentialForceScaler.value;
+        tangentialForceRadius = uiCanvas.tangentialForceRadius.value;
+
+        radialForceIsOn = uiCanvas.radialForceToggle.isOn;
+        radialForceScaler = uiCanvas.radialForceScaler.value;
+        radialForceRadius = uiCanvas.radialForceRadius.value;
+
         nu = u0 * DIM / Re;
         tau = 3.0f * nu + 0.5f;
         omega = 1.0f / tau;
         compute.SetFloat("rho0",rho0);
         compute.SetFloat("tau",tau);
-        compute.SetFloat("forceRadius",forceRadius);
-        compute.SetFloat("forceScaler",forceScaler);
         compute.SetFloat("u0",u0);
+        compute.SetFloat("tangentialForceRadius",tangentialForceRadius);
+        compute.SetFloat("tangentialForceScaler",tangentialForceScaler);
+        compute.SetBool("tangentialForceIsOn",tangentialForceIsOn);
+        compute.SetFloat("radialForceRadius",radialForceRadius);
+        compute.SetFloat("radialForceScaler",radialForceScaler);
+        compute.SetBool("radialForceIsOn",radialForceIsOn);
     }
 }
--- Assets/Scripts/UICanvas.cs
+++ Assets/Scripts/UICanvas.cs
@@ -2,17 +2,94 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UICanvas : MonoBehaviour
 {
     public LBM3DAccurate lbm3d;
     public Toggle tangentialForceToggle;
+    public TextMeshProUGUI tangentialForceScalerText;
     public Slider tangentialForceScaler;
+    public TextMeshProUGUI tangentialForceRadiusText;
     public Slider tangentialForceRadius;
+    public Toggle radialForceToggle;
+    public TextMeshProUGUI radialForceScalerText;
+    public Slider radialForceScaler;
+    public TextMeshProUGUI radialForceRadiusText;
+    public Slider radialForceRadius;
+    public Camera sideCamera;
+    public Camera topCamera;
+    public GameObject constantForceMenu;
+    public GameObject touchForceMenu;
+
+    void Start()
+    {
+        tangentialForceScalerText.text = "Force Scaler : " + lbm3d.tangentialForceScaler.ToString();
+        radialForceScalerText.text = "Force Radius : " + lbm3d.radialForceScaler.ToString();
+        tangentialForceRadiusText.text = "Force Scaler : " + lbm3d.tangentialForceRadius.ToString();
+        radialForceRadiusText.text = "Force Radius : " + lbm3d.radialForceRadius.ToString();
+    }
 
     public void OnToggleChange(int forceType)
     {
-        lbm3d.tangentialForceIsOn = tangentialForceToggle.isOn;
-        lbm3d.compute.SetBool("tangentialForceIsOn",lbm3d.tangentialForceIsOn);
+        if(forceType == 0)
+        {
+            lbm3d.tangentialForceIsOn = tangentialForceToggle.isOn;
+            lbm3d.compute.SetBool("tangentialForceIsOn",lbm3d.tangentialForceIsOn);
+        }
+        else
+        {
+            lbm3d.radialForceIsOn = radialForceToggle.isOn;
+            lbm3d.compute.SetBool("radialForceIsOn",lbm3d.radialForceIsOn);
+        }
+    }
+    
+    public void OnScalerSliderChange(int forceType)
+    {
+        if(forceType == 0)
+        {
+            lbm3d.tangentialForceScaler = tangentialForceScaler.value;
+            lbm3d.compute.SetFloat("tangentialForceScaler",lbm3d.tangentialForceScaler);
+            tangentialForceScalerText.text = "Force Scaler : " + lbm3d.tangentialForceScaler.ToString();
+        } 
+        else
+        {
+            lbm3d.radialForceScaler = radialForceScaler.value;
+            lbm3d.compute.SetFloat("radialForceScaler",lbm3d.radialForceScaler);
+            radialForceScalerText.text = "Force Scaler : " + lbm3d.radialForceScaler.ToString();
+        }
+    }
+
+    public void OnRadiusSliderChange(int forceType)
+    {
+        if(forceType == 0)
+        {
+            lbm3d.tangentialForceRadius = tangentialForceRadius.value;
+            lbm3d.compute.SetFloat("tangentialForceRadius",lbm3d.tangentialForceRadius);
+            tangentialForceRadiusText.text = "Force Radius : " + lbm3d.tangentialForceRadius.ToString();
+        }
+        else
+        {
+            lbm3d.radialForceRadius = radialForceRadius.value;
+            lbm3d.compute.SetFloat("radialForceRadius",lbm3d.radialForceRadius);
+            radialForceRadiusText.text = "Force Radius : " + lbm3d.radialForceRadius.ToString();
+        }
+    }
+
+    public void Initialize()
+    {
+        lbm3d.Initialize();
+    }
+
+    public void SwitchCamera()
+    {
+        sideCamera.enabled = topCamera.enabled;
+        topCamera.enabled = !topCamera.enabled;
+    }
+
+    public void SwitchForceMenu()
+    {
+        constantForceMenu.SetActive(touchForceMenu.activeSelf);
+        touchForceMenu.SetActive(!touchForceMenu.activeSelf);
     }
 }
